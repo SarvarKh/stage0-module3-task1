d3f88f3fd1df200091e96a6c970fe10b0f57fa9e
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-17 14:57:05 +0500

    Resolve mis-described assignment No.5 Update class

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task1;
 public class UpdatedApp {
     public static void main(String[] args) {
         System.out.println(
-                "I'm" + "\n" + "updated" + "\n" + "can" + "\n" + "be" + "\n" + "compiled." + "\n" + "And" + "\n" + "can" + "\n" + "write" + "\n" + "to" + "\n" + "console"
+                "I'm updated" + "\n" + "can be compiled. And can write to console"
         );
     }
 }
